feabf7c2e2fc9837bf461684261775b53ce7ecb1
Z0ro0 <w2110@e-mirim.hs.kr>
2023-03-09 11:52:46 +0900

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+		String[] beanNames = context.getBeanDefinitionNames();
+		for(String name: beanNames){
+			if(!(name.startsWith("org.")|| name.startsWith("spring."))) {
+				System.out.println(name);
+			}
+		}
+		MyBean bean = (MyBean) context.getBean("myBean");
+		System.out.println(bean);
+		Person p = (Person) context.getBean((Person.class));
+		System.out.println(p);
+	}
+}
---
+++ src/main/java/com/example/demo/MyBean.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component//클래스 위에 붙이면 빈으로 등록됨
+public class MyBean {
+
+}
